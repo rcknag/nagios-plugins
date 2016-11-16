d2b592f26bfd314ecf39699f8a5400d0b50533fa
Clint Kennedy <rkennedy@nagios.com>
2016-11-16 16:43:08 -0600

    Updated to have better status and crit option.

--- check_hpprocurve.sh
+++ check_hpprocurve.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # check_hpprocurve.sh
 # Copyright (C) 2016 Nagios Enterprises, LLC
-# Version 1.0 - 04/26/2016
+# Version 1.1 - 11/15/2016
 # Questions/issues should be posted on the Nagios
 # Support Forum at https://support.nagios.com/forum/
 # Feedback/recommendations/tips can be sent to
-# Robert Kennedy (Clint) rkennedy@nagios.com
+# Clint Kennedy rkennedy@nagios.com
 
 # Help menu
 print_help() {
@@ -17,16 +17,18 @@ echo "-H        Host"
 echo "-t        Check type (1 (load), 2 (uptime), 3 (interface status - requires i)"
 echo "-t        Check type 4 (IfInBroad), 5 (IfOutBroad), 6 (IfOutMulti), 7 (IfInMulti), 8 (Excess Collissions)"
 echo "-i        Interface (if required by type of check)"
+echo "-C		Critical threshold (use to check if greater than or equal to)"
 echo "-h        Help menu"
 echo ""
 echo "Example:"
 echo "check_hpprocurve.sh -v 2c -c public -H 1.2.3.4 -t 1"
 echo "check_hpprocurve.sh -v 2c -c public -H 1.2.3.4 -t 3 -i 21"
+echo "check_hpprocurve.sh -v 2c -c public -H 1.2.3.4 -t 8 -i 21 -C 1"
 exit 0
 }
 
 # Define input vars
-while getopts "v:c:H:t:i:h" option
+while getopts "v:c:H:t:i:C:h" option
 do
         case $option in
                 v) vers=$OPTARG ;;
@@ -34,6 +36,7 @@ do
                 H) host=$OPTARG ;;
                 t) type=$OPTARG ;;
                 i) int=$OPTARG;;
+				C) crit=$OPTARG;;
                 h) print_help 0
                 exit 0 ;;
         esac
@@ -42,36 +45,81 @@ done
 # Check input vars
 if [ -z "$vers" ] || [ -z "$comm" ] || [ -z "$host" ] || [ -z "$type" ]; then
 echo "Invalid parameters. Script requires a SNMP version, community string, host, and type. To access the help menu use the -h flag."
-exit 0
-elif [ $type -eq 3 ] && [ -z "$int" ]; then
+exit 2
+elif [ $type -ge 3 ] && [ -z "$int" ]; then
 echo "No interface number specified"
-exit 0
+exit 2
 fi
 
 # Run check based on type
+
+#CPU
 if [ $type -eq 1 ]; then
-load=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.4.1.11.2.14.11.5.1.9.6.1.0)
-echo "$load"
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.4.1.11.2.14.11.5.1.9.6.1.0)
+checkresult=
+
+#Uptime 
 elif [ $type -eq 2 ]; then
-upti=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.1.3.0)
-echo "$upti"
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.1.3.0)
+uptimedays=$(echo $check | cut -d " " -f 5)
+uptimehms=$(echo $check | cut -d " " -f 7)
+critalert=$uptimedays
+checkresult="Uptime is $uptimedays days $uptimehms"
+
+#ifOperStatus
 elif [ $type -eq 3 ]; then
-intstat=$(snmpwalk -v $vers -c $comm $host ifOperStatus.$int)
-echo "$intstat"
-elif [ $type -eq 4 ]; then
-ifinbroad=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.3.$int)
-echo "$ifinbroad"
-elif [ $type -eq 5 ]; then
-ifoutbroad=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.5.$int)
-echo "$ifoutbroad"
-elif [ $type -eq 6 ]; then
-ifoutmulti=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.4.$int)
-echo "$ifoutmulti"
-elif [ $type -eq 7 ]; then
-ifinmulti=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.2.$int)
-echo "$ifinmulti"
-elif [ $type -eq 8 ]; then
-excesscoll=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.10.7.2.1.9.$int)
-echo "$excesscoll"
+check=$(snmpwalk -v $vers -c $comm $host ifOperStatus.$int)
+upordown=$(echo $check | grep 'up' | wc -l)
+if [ $upordown -eq 1 ]; then
+checkresult="Interface $int is up."
+critalert="0"
+else
+checkresult="Interface $int is unknown."
+critalert="1"
 fi
-exit 0
+
+#ifInBroadcastPkts
+elif [ $type -eq 4 ]; then
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.3.$int)
+critalert=$(echo $check | cut -d " " -f 4)
+checkresult="ifInBroadcastPkts on interface $int is $critalert"
+
+#ifOutBroadcastPkts
+elif [ $type -eq 5 ]; then
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.5.$int)
+critalert=$(echo $check | cut -d " " -f 4)
+checkresult="ifOutBroadcastPkts on interface $int is $critalert"
+
+#ifOutMulticastPkts
+elif [ $type -eq 6 ]; then
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.4.$int)
+critalert=$(echo $check | cut -d " " -f 4)
+checkresult="ifOutMulticastPkts on interface $int is $critalert"
+
+#ifInMulticastPkts
+elif [ $type -eq 7 ]; then
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.31.1.1.1.2.$int)
+critalert=$(echo $check | cut -d " " -f 4)
+checkresult="ifInMulticastPkts on interface $int is $critalert"
+
+#Excessive Collisions
+elif [ $type -eq 8 ]; then
+check=$(snmpwalk -v $vers -c $comm $host 1.3.6.1.2.1.10.7.2.1.9.$int)
+critalert=$(echo $check | cut -d " " -f 4)
+checkresult="Excessive collisions on interface $int is $critalert"
+fi
+
+#Check if crit var is empty
+if [ -z "$crit" ]; then
+state="OK"
+stateexit="0"
+elif [ $critalert -ge $crit ]; then
+state="CRITICAL"
+stateexit="2"
+else
+state="OK"
+stateexit="0"
+fi
+
+echo "$state: $checkresult"
+exit $stateexit
